caec9cd10b2eb1bd4e3d9ffeb90ae000c05137e9
A. Esad AY <esad@bayzat.com>
2022-06-08 01:23:56 +0300

    Fix terraform key config

--- tf/main.tf
+++ tf/main.tf
@@ -8,5 +8,7 @@ terraform {
 
 #It is not possible to assume role with data.aws_iam_role
 provider "aws" {
-  region = "eu-west-1"
+  region     = "eu-west-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
