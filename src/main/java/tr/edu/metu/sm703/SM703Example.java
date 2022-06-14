89b9da44f3f43b4ae2049f777a341f188419cc24
A. Esad AY <esad@bayzat.com>
2022-06-15 01:32:17 +0300

    Change body handling logic

--- src/main/java/tr/edu/metu/sm703/SM703Example.java
+++ src/main/java/tr/edu/metu/sm703/SM703Example.java
@@ -24,18 +24,39 @@ public class SM703Example implements RequestStreamHandler {
 
     @Override
     public void handleRequest(InputStream input, OutputStream output, Context context) {
-
+        Integer o1 = null, o2 = null;
+        JSONObject errorObject = null;
         PrintWriter writer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(output, Charset.forName("US-ASCII"))));
         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
             Object obj = JSONValue.parse(reader);
             JSONObject event = (JSONObject)obj;
 
-            String bodyString = (String)event.get("body");
+            if(event.containsKey("o1") && event.containsKey("o2")){
+                o1 = ((Number)event.get("o1")).intValue();
+                o2 = ((Number)event.get("o2")).intValue();
+            }
+            else{
+                String bodyString = (String)event.get("body");
+                JSONObject body = (JSONObject)JSONValue.parse(bodyString);
+                if(body.containsKey("o1") && body.containsKey("o2") ){
+                    o1 = ((Number)body.get("o1")).intValue();
+                    o2 = ((Number)body.get("o2")).intValue();
+                }
+            }
 
-            JSONObject body = (JSONObject)JSONValue.parse(bodyString);
-            Integer o1 = ((Number)body.get("o1")).intValue();
-            Integer o2 = ((Number)body.get("o2")).intValue();
+            
+            if(isNull(o1) && isNull(o2)){
+                errorObject = new JSONObject();
+                errorObject.put("statusCode", 400);
+                errorObject.put("isBase64Encoded", false);
+                errorObject.put("headers", null);
+                errorObject.put("body", "Invalid request. You need to feed atleast o1 or o2 value!");
+                writer.write(errorObject.toString());
+                writer.close();
+                return;
+            }
 
+        
         JSONObject responseObject = new JSONObject();
 
         responseObject.put("statusCode", 200);
